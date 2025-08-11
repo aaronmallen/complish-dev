9190401d2d3ea83129fbf5c31ee56f801ab6dd83
Aaron Allen <hello@aaronmallen.me>
2025-08-11 17:23:39 -0500

    Add `services::VaultService`

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,9 @@
 mod project_document_service;
 mod task_document_service;
 mod toml_loader_service;
+mod vault_service;
 
 pub use project_document_service::ProjectDocumentService;
 pub use task_document_service::TaskDocumentService;
 pub use toml_loader_service::TomlLoaderService;
+pub use vault_service::VaultService;
