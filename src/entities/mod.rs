94b9205546825e9e656c6bfd8d0e90a5d8f05929
Aaron Allen <hello@aaronmallen.me>
2025-08-11 13:15:51 -0500

    Add `entities::Project`

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,9 +1,11 @@
 mod priority;
+mod project;
 mod task;
 mod task_list;
 mod task_status;
 
 pub use priority::Priority;
+pub use project::Project;
 pub use task::Task;
 pub use task_list::TaskList;
 pub use task_status::TaskStatus;
