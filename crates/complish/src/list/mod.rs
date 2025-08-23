ca232797377da3ac3a2e812614425e006a537ec7
Aaron Allen <hello@aaronmallen.me>
2025-08-23 18:04:20 -0500

    Add `list::repo`
    Introduces a `Repo` struct as the main entry point for database
    operations in Complish. Manages the SQLite connection and provides
    access to specialized repositories like `list::repo`.
    
    Includes a `Repo::new` method for initializing the repository,
    a `list()` method for accessing list operations, and comprehensive
    tests to validate functionality. Integrates the `repo` module
    into `vault`.

--- crates/complish/src/list/mod.rs
+++ crates/complish/src/list/mod.rs
@@ -1,5 +1,8 @@
+pub(crate) mod repo;
+
 use chrono::{DateTime, Utc};
 use getset::Getters;
+use rusqlite::{Error as SqliteError, Result as SqliteResult, Row};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, Getters, PartialEq, Serialize)]
@@ -12,4 +15,57 @@ pub struct List {
   name: String,
   #[get = "pub"]
   updated_at: DateTime<Utc>,
+}
+
+impl TryFrom<&Row<'_>> for List {
+  type Error = SqliteError;
+
+  fn try_from(row: &Row<'_>) -> SqliteResult<Self> {
+    Ok(List {
+      created_at: row.get("created_at")?,
+      id: row.get("id")?,
+      name: row.get("name")?,
+      updated_at: row.get("updated_at")?,
+    })
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use rusqlite::{Connection, Result as SqliteResult};
+  use temp_dir::TempDir;
+
+  use super::*;
+  use crate::vault::migrations;
+
+  fn get_connection() -> Connection {
+    let temp_dir = TempDir::new().unwrap();
+    let vault_path = temp_dir.path().join("vault");
+    migrations::run(&vault_path).unwrap();
+    Connection::open(vault_path).unwrap()
+  }
+
+  mod try_from {
+    use super::*;
+
+    #[test]
+    fn it_converts_row_to_list() {
+      let connection = get_connection();
+      let mut statement = connection
+        .prepare("SELECT * FROM lists WHERE id = 1")
+        .unwrap();
+      let list: List = statement.query_row([], |row| List::try_from(row)).unwrap();
+
+      assert_eq!(list.name(), "Today");
+    }
+
+    #[test]
+    fn it_handles_missing_columns() {
+      let connection = get_connection();
+      let mut statement = connection.prepare("SELECT 1 as dummy").unwrap();
+      let result: SqliteResult<List> = statement.query_row([], |row| List::try_from(row));
+
+      assert!(result.is_err());
+    }
+  }
 }
---
+++ crates/complish/src/list/repo.rs
@@ -0,0 +1,107 @@
+use rusqlite::{Connection, Result as SqliteResult};
+
+use crate::List;
+
+pub struct Repo<'a> {
+  connection: &'a Connection,
+}
+
+impl<'a> Repo<'a> {
+  pub const SELECT_BY_NAME_SQL: &'static str = r"
+    SELECT * FROM lists WHERE LOWER(name) = LOWER(?1)
+  ";
+  pub const SELECT_BY_PK_SQL: &'static str = r"
+    SELECT * FROM lists WHERE id = ?1
+  ";
+
+  pub fn new(connection: &'a Connection) -> Self {
+    Self {
+      connection,
+    }
+  }
+
+  pub fn by_name(&self, name: impl Into<String>) -> SqliteResult<List> {
+    let mut statement = self.connection.prepare(Self::SELECT_BY_NAME_SQL)?;
+
+    statement.query_row([name.into()], |row| List::try_from(row))
+  }
+
+  pub fn by_pk(&self, id: u32) -> SqliteResult<List> {
+    let mut statement = self.connection.prepare(Self::SELECT_BY_PK_SQL)?;
+
+    statement.query_row([id], |row| List::try_from(row))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use rusqlite::Connection;
+  use temp_dir::TempDir;
+
+  use crate::vault::migrations;
+
+  fn get_test_connection() -> Connection {
+    let temp_dir = TempDir::new().unwrap();
+    let vault_path = temp_dir.path().join("vault");
+    migrations::run(&vault_path).unwrap();
+    Connection::open(vault_path).unwrap()
+  }
+
+  mod by_name {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+    use crate::list::repo::Repo;
+
+    #[test]
+    fn it_returns_the_list_by_name() {
+      let connection = get_test_connection();
+      let repo = Repo::new(&connection);
+      let today = repo.by_name("today").unwrap();
+      let next = repo.by_name("next").unwrap();
+      let someday = repo.by_name("someday").unwrap();
+
+      assert_eq!(today.name, "Today");
+      assert_eq!(next.name, "Next");
+      assert_eq!(someday.name, "Someday");
+    }
+
+    #[test]
+    fn it_returns_an_error_if_the_list_does_not_exist() {
+      let connection = get_test_connection();
+      let repo = Repo::new(&connection);
+      let result = repo.by_name("does not exist");
+
+      assert!(result.is_err());
+    }
+  }
+
+  mod by_pk {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+    use crate::list::repo::Repo;
+
+    #[test]
+    fn it_returns_the_list_by_pk() {
+      let connection = get_test_connection();
+      let repo = Repo::new(&connection);
+      let today = repo.by_pk(1).unwrap();
+      let next = repo.by_pk(2).unwrap();
+      let someday = repo.by_pk(3).unwrap();
+
+      assert_eq!(today.name, "Today");
+      assert_eq!(next.name, "Next");
+      assert_eq!(someday.name, "Someday");
+    }
+
+    #[test]
+    fn it_returns_an_error_if_the_list_does_not_exist() {
+      let connection = get_test_connection();
+      let repo = Repo::new(&connection);
+      let result = repo.by_pk(4);
+
+      assert!(result.is_err());
+    }
+  }
+}
